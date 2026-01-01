ee38a2ac444bd1f4431acf9c92ed21a53c9a571d
Rashid Narikkodan <rashidnarikkodan20@gmail.com>
2026-01-01 13:28:13 +0530

    chore: add core domain types

---
+++ src/types/movie.ts
@@ -0,0 +1,8 @@
+export interface Movie {
+  id: string
+  title: string
+  overview: string
+  posterUrl: string
+  rating: number
+  genres: string[]
+}
---
+++ src/types/watchlist.ts
@@ -0,0 +1,5 @@
+import type { Movie } from "./movie"
+
+export interface WatchlistState {
+  movies: Movie[]
+}
